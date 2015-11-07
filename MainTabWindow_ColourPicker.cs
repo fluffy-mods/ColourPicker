bfafec5ecfb0dd306357b22caba7a79ecfcc38a9
Karel Kroeze <karel.kroeze@gmail.com>
2015-11-07 18:42:18 +0100

    Done

---
+++ MainTabWindow_ColourPicker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace ColourPicker
+{
+    class MainTabWindow_ColourPicker : MainTabWindow
+    {
+        public ColourWrapper    BGCol;
+        public Texture2D        BGTex;
+
+        public MainTabWindow_ColourPicker()
+        {
+            BGCol = new ColourWrapper(Color.grey);
+            BGTex = SolidColorMaterials.NewSolidColorTexture( BGCol.Color );
+        }
+        
+        public override void DoWindowContents( Rect inRect )
+        {
+            GUI.DrawTexture( inRect, BGTex );
+            Rect button = new Rect(0f, 0f, 200f, 35f);
+            button = button.CenteredOnXIn( inRect ).CenteredOnXIn( inRect );
+
+            if (Widgets.TextButton(button, "Change Colour" ) )
+            {
+                Find.WindowStack.Add( new Dialog_ColourPicker( BGCol, delegate { BGTex = SolidColorMaterials.NewSolidColorTexture( BGCol.Color ); } ) );
+            }
+        }
+    }
+}
